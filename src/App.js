ae1bebb2e76b595add8718b677342a29545b38e8
Daniel Cornejo <mr.daniel.cornejo@gmail.com>
2022-12-11 19:29:03 -0500

    styling and prelim function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
+import { useState, useEffect } from "react";
+import "./styles.css";
+import INGREDIENTS from "./ingredients";
+import BurritoBuilder from "./BurritoBuilder";
+
+export default function App() {
+  const [allIngredients, setAllIngredients] = useState(INGREDIENTS);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test <code>src/App.js</code> and test to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BurritoBuilder allIngredients={allIngredients} />
     </div>
   );
 }
-
-export default App;
